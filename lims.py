08d0cd2365d16913e6884123180ef3cf65249532
Jack <jyen@makomedical.com>
2016-02-18 09:31:01 -0500

    added some comments

--- lims.py
+++ lims.py
@@ -1,3 +1,11 @@
+# #===========================================================================##
+# # lims.py
+# # This is a test script to test the LIMS API python package
+# #
+# # Jack Yen
+# # Feb 18th, 2015
+# #===========================================================================##
+
 from genologics.lims import *
 from genologics.config import BASEURI, USERNAME, PASSWORD
 
